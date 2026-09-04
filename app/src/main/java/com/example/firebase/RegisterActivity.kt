083a510c076d91eb97e2fff0bd93b8503d072d55
AlejandroFranco1101 <alejandrofranco1101@gmail.com>
2026-09-03 18:43:08 -0600

    Logica  de registro con Firebase Authentication y la navegación
    Inicialización de la instancia de FirebaseAuth.getInstance()

--- app/src/main/java/com/example/firebase/RegisterActivity.kt
+++ app/src/main/java/com/example/firebase/RegisterActivity.kt
@@ -1,12 +1,26 @@
 package com.example.firebase
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+
+    // Referencia del objeto FirebaseAuth
+    private lateinit var auth: FirebaseAuth
+
+    // Referencia a componentes del layout
+    private lateinit var buttonRegister: Button
+    private lateinit var textViewLogin: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +30,42 @@ class RegisterActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Inicializacion del objeto FirebaseAuth
+        auth = FirebaseAuth.getInstance()
+
+        buttonRegister = findViewById(R.id.btnRegister)
+        buttonRegister.setOnClickListener {
+            val email = findViewById<EditText>(R.id.txtEmail).text.toString()
+            val password = findViewById<EditText>(R.id.txtPassword).text.toString()
+            this.register(email, password)
+        }
+
+        textViewLogin = findViewById(R.id.textViewLogin)
+        textViewLogin.setOnClickListener {
+            this.goToLogin()
+        }
+    }
+
+    private fun register(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }.addOnFailureListener { exception ->
+                Toast.makeText(
+                    applicationContext,
+                    exception.localizedMessage,
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+    }
+
+    private fun goToLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
